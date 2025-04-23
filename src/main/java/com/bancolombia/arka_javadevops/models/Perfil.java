eeebcc41e8c52ac26d71c8c5e123033737c792f6
Daniel Lopez <daniellop199731@gmail.com>
2025-04-23 14:02:50 -0500

    Creacion de modelo de dominio
    Se crea en un package llamado models
    @Entity //Indica a Spring que esta clase es un bean y que refleja informacion de la base de datos
    @Table(name = "perfiles") //Se especifica el nombre de la base de datos
    @Id //Indica que este atributo es el ID de la tabla
    @GeneratedValue(strategy = GenerationType.AUTO) //Indica que el atributo Id es autoincrementable
    @Column(name = "nombrePerfil") //Si el atributo se llama igual a la columna de la tabla la anotacion @Column no es necesaria
    Para JPA es necesarios un constructor vacio
    
    Se corrije el pom, ya que la importacion pasada de JPA quedo mala

---
+++ src/main/java/com/bancolombia/arka_javadevops/models/Perfil.java
@@ -0,0 +1,43 @@
+package com.bancolombia.arka_javadevops.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //Indica a Spring que esta clase es un bean y que refleja informacion de la base de datos
+@Table(name = "perfiles") //Se especifica el nombre de la base de datos
+public class Perfil {
+
+    @Id //Indica que este atributo es el ID de la tabla
+    @GeneratedValue(strategy = GenerationType.AUTO) //Indica que el Id es autoincrementable
+    private int idPerfil;
+
+    //Si el atributo se llama igual a la columna de la tabla la anotacion @Column 
+    //no es necesaria
+    @Column(name = "nombrePerfil")                                 
+    private String nombrePerfil; 
+
+    //Para JPA es necesarios un constructor vacio
+    public Perfil() {
+    }
+
+    public int getIdPerfil() {
+        return idPerfil;
+    }
+
+    public void setIdPerfil(int idPerfil) {
+        this.idPerfil = idPerfil;
+    }
+
+    public String getNombrePerfil() {
+        return nombrePerfil;
+    }
+
+    public void setNombrePerfil(String nombrePerfil) {
+        this.nombrePerfil = nombrePerfil;
+    }
+    
+}
