2c78e11dcddb06ec9fb840dc2a30cae651f2735f
Daniel Lopez <daniellop199731@gmail.com>
2025-04-22 19:21:27 -0500

    Controlador y URI de prueba

---
+++ src/main/java/com/bancolombia/arka_javadevops/controllers/MainController.java
@@ -0,0 +1,19 @@
+package com.bancolombia.arka_javadevops.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping("/test1")
+    public String holaMundo() {
+        return "Hola mundo";
+    }
+    
+
+}
