59d98b8e41761632479af6681dafdd9a807ab5d0
Daniel Lopez <daniellop199731@gmail.com>
2025-04-23 14:16:35 -0500

    Creacion de PerfilRepository
    @Repository //Indica que la interfaz es un bean de Spring y que representa el acceso a los datos
    extends CrudRepository<Perfil, Integer> // Al extender de la interfaz CrudRepository se obtienen metodos de acceso de datos basicos como crear, buscar, y eliminar, se pasa la clase Entity y el tiempo de datos de su Id

---
+++ src/main/java/com/bancolombia/arka_javadevops/repositories/PerfilRepository.java
@@ -0,0 +1,26 @@
+package com.bancolombia.arka_javadevops.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bancolombia.arka_javadevops.models.Perfil;
+
+@Repository
+public interface PerfilRepository extends CrudRepository<Perfil, Integer>{
+
+  // Aquí se pueden agregar métodos adicionales de consulta personalizada si es necesario
+  //Ejemplo
+  List<Perfil> findByNombre(String nombrePerfil);
+  
+  /*
+   * Con solo extender CrudRepository, obtienes automaticamente métodos como:
+   * 
+   * findById(Long id): para buscar por ID.
+   * findAll(): para obtener todos los registros.
+   * save(Objeto obj): para guardar o actualizar un producto.
+   * deleteById(Long id): para eliminar un producto por su ID. 
+   */
+
+}
