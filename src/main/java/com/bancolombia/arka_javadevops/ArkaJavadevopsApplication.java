45763c850bf1d6129873350f068793d7e7f02350
Daniel Lopez <daniellop199731@gmail.com>
2025-04-22 19:13:36 -0500

    Creacion del proyecto arka

---
+++ src/main/java/com/bancolombia/arka_javadevops/ArkaJavadevopsApplication.java
@@ -0,0 +1,13 @@
+package com.bancolombia.arka_javadevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaJavadevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaJavadevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancolombia/arka_javadevops/ArkaJavadevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.arka_javadevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaJavadevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
