701dfbb5b8fcff757bb2835af691c81c7c22573b
Daniel Lopez <daniellop199731@gmail.com>
2025-04-24 21:19:40 -0500

    Manejo de ResponseEntity

--- src/main/java/com/bancolombia/arka_javadevops/controllers/MainController.java
+++ src/main/java/com/bancolombia/arka_javadevops/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.bancolombia.arka_javadevops.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +18,30 @@ public class MainController {
         return "Hola mundo";
     }
 
+    @GetMapping("/respuesta")
+    public ResponseEntity<String> respuesta() {
+        return new ResponseEntity<>("Esta es mi respuesta", HttpStatus.ACCEPTED);
+    }
+    
+
     @GetMapping("/buscar")
-    public String buscar(@RequestParam(value = "q", required = false) String busqueda) {
-        return "Estas buscando " + busqueda;
+    public ResponseEntity<String> buscar(@RequestParam(value = "q", required = false) String busqueda) {
+
+        if(busqueda != null){
+            return new ResponseEntity<>("Estas buscando " + busqueda, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Falta el campo q ", HttpStatus.BAD_REQUEST);
+        }
+
+        
     }
 
     /*
      * Ejemplo de URL en postman: http://localhost:8080/main/usuarios/1040755872
      */
     @GetMapping("/usuarios/{id}")
-    public String mostrar(@PathVariable("id") String idUsuario) {
-        return "Se solicitó info del usuario con  id: " + idUsuario;
+    public ResponseEntity<String> mostrar(@PathVariable("id") String idUsuario) {
+        return new ResponseEntity<>("Se solicitó info del usuario con  id: " + idUsuario, HttpStatus.OK);
     }
     
     
