ce06a9ee71e5f69f1741d7c89607df47f7829b47
Daniel Lopez <daniellop199731@gmail.com>
2025-05-14 15:40:53 -0500

    Prueba completa de MetodoPagoController

--- src/test/java/com/bancolombia/arka_javadevops/controllers/MetodoPagoControllerTest.java
+++ src/test/java/com/bancolombia/arka_javadevops/controllers/MetodoPagoControllerTest.java
@@ -1,6 +1,9 @@
 package com.bancolombia.arka_javadevops.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -36,8 +39,21 @@ public class MetodoPagoControllerTest {
         when(metodoPagoService.obtenerMetodosPago())
             .thenReturn(List.of(new MetodoPago(1, "nuevo", null, null)));
         ResponseEntity<List<MetodoPago>> responseEntity = metodoPagoController.obtenerMetodosPago();
+
+        //Valida el codigo de la respuesa
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        
+        //Valida que el ResponseEntity si tenga algo diferente de null
+        assertNotNull(responseEntity.getBody());
+        //Como dijimos que se devuelve un solo el elemento de la lista, se valida que solo se 
+        //devuelva un elemento
+        assertEquals(1, responseEntity.getBody().size());
+        //Valida el id del elemento que se devuelve
+        assertEquals(1, responseEntity.getBody().get(0).getIdMetodoPago());
+        //Valida el nombre del mentodo de pago que se devuelve
+        assertEquals("nuevo", responseEntity.getBody().get(0).getNombreMetodoPago());
+        //valida que las listas sean nullas
+        assertNull(responseEntity.getBody().get(0).getMetodosPagoUsuario());
+        assertNull(responseEntity.getBody().get(0).getCarritosCompras());
     }
 
 }
